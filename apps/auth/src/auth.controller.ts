cecda4d60944032583daeabf0a391446e89fe047
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-06-27 23:18:42 +0200

    feat: implement login controller method

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Response } from 'express';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 
-@Controller()
+import { AuthService } from './auth.service';
+import { UserDocument } from './users/models/user.schema';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './decorators/current-user.decorator';
+
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @CurrentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    await this.authService.login(user, response);
+    response.send(user);
   }
 }
