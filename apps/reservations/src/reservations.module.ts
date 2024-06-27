0bb4ffeaabaf5667b5e00dd0b0ff58239e1adcad
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-06-27 23:24:55 +0200

    feat: implement reservation config

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule, LoggerModule } from '@app/common';
+import * as Joi from 'joi';
 
 import {
   ReservationDocument,
@@ -8,6 +9,7 @@ import {
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsRepository } from './reservations.repository';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -16,6 +18,13 @@ import { ReservationsRepository } from './reservations.repository';
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
