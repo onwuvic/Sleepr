ba523e3c86c98d8f4845176ce59e89a05fbd965e
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-06-27 23:20:06 +0200

    feat: add jwt and config module to auth

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,11 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '@app/common';
+import * as Joi from 'joi';
+
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { UsersModule } from './users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    LoggerModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get<string>('JWT_TOKEN_EXPIRATION')}s`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_TOKEN_EXPIRATION: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
