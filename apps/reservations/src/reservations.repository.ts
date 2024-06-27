558e57ecec1a7b55ed7fd10020c80c97b0a199b1
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-06-27 11:58:01 +0200

    feat: create reservation app

---
+++ apps/reservations/src/reservations.repository.ts
@@ -0,0 +1,18 @@
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { AbstractRepository } from '@app/common';
+import { Injectable, Logger } from '@nestjs/common';
+
+import { ReservationDocument } from './models/reservation.schema';
+
+@Injectable()
+export class ReservationsRepository extends AbstractRepository<ReservationDocument> {
+  protected readonly logger = new Logger(ReservationsRepository.name);
+
+  constructor(
+    @InjectModel(ReservationDocument.name)
+    reservationModel: Model<ReservationDocument>,
+  ) {
+    super(reservationModel);
+  }
+}
