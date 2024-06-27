5011710843ac8864210a19444077f6128f427feb
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-06-27 23:21:57 +0200

    feat: access port from config service

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,13 +1,16 @@
 import { Logger } from 'nestjs-pino';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  const configService = app.get(ConfigService);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(3001);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,13 +1,16 @@
 import { Logger } from 'nestjs-pino';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 import { ReservationsModule } from './reservations.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  const configService = app.get(ConfigService);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
