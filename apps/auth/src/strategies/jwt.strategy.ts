7bea3645f4808a40a618a3a7dc3a41aa5c74202d
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-06-28 15:32:58 +0200

    feat: create jwt strategy

---
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { ConfigService } from '@nestjs/config';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+import { UsersService } from '../users/users.service';
+import { TokenPayload } from '../interfaces/token-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly usersService: UsersService,
+    configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: any) =>
+          // the first is for HTTP and the second is for TCP request
+          request?.cookies?.Authentication || request?.Authentication,
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate({ userId }: TokenPayload) {
+    return this.usersService.getUser({ _id: userId });
+  }
+}
