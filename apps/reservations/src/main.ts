f5b4becf2a2abb00f0cbe77382f4671da4ec8d95
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-06-28 15:36:50 +0200

    feat: add cookie parser to reservation service

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,6 +2,7 @@ import { Logger } from 'nestjs-pino';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 import { ReservationsModule } from './reservations.module';
 
@@ -9,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   const configService = app.get(ConfigService);
 
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   await app.listen(configService.get('PORT'));
