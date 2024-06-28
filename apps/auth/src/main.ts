e96d4081e078a068d0c6e3c01c04b9621c64030b
Onwuzor Victor <onwuzorvictor@outlook.com>
2024-06-28 15:34:32 +0200

    feat: auth set up open connect to its service

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,6 +2,8 @@ import { Logger } from 'nestjs-pino';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import * as cookieParser from 'cookie-parser';
 
 import { AuthModule } from './auth.module';
 
@@ -9,8 +11,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   const configService = app.get(ConfigService);
 
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('TCP_PORT'),
+    },
+  });
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(configService.get('PORT'));
+  await app.startAllMicroservices();
+  await app.listen(configService.get('HTTP_PORT'));
 }
 bootstrap();
