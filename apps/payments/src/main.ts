d6ad58488903e2911d125f4bbc24ba42fd3d3059
Onwuzor Victor <victor.onwuzor@cozero.io>
2024-07-12 08:48:33 +0200

    feat: payment main entry file
    Signed-off-by: Onwuzor Victor <victor.onwuzor@cozero.io>

---
+++ apps/payments/src/main.ts
@@ -0,0 +1,24 @@
+import { Logger } from 'nestjs-pino';
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
+
+import { PaymentsModule } from './payments.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(PaymentsModule);
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('PORT'),
+    },
+  });
+
+  app.useLogger(app.get(Logger));
+  await app.startAllMicroservices();
+  // await app.listen(configService.get('PORT'));
+}
+bootstrap();
