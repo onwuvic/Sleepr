a5964f6f87b58fd0525db35c98fb82aa8317175c
Onwuzor Victor <victor.onwuzor@cozero.io>
2024-10-06 15:43:10 +0200

    feat: add notification service consumer to payment

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { LoggerModule } from '@app/common';
-import { ConfigModule } from '@nestjs/config';
+import { LoggerModule, NOTIFICATIONS_SERVICE } from '@app/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -13,8 +14,23 @@ import { PaymentsService } from './payments.service';
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
         STRIPE_SECRET_KEY: Joi.string().required(),
+        NOTIFICATION_HOST: Joi.string().required(),
+        NOTIFICATION_PORT: Joi.number().required(),
       }),
     }),
+    ClientsModule.registerAsync([
+      {
+        name: NOTIFICATIONS_SERVICE,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configService.get('NOTIFICATION_HOST'),
+            port: configService.get('NOTIFICATION_PORT'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
     LoggerModule,
   ],
   controllers: [PaymentsController],
