6b2e45f97630aca2e63803623993ed20da2d2e03
Onwuzor Victor <victor.onwuzor@cozero.io>
2024-11-23 16:13:52 +0100

    feat: add email notification service endpoint

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,10 +1,40 @@
 import { Injectable } from '@nestjs/common';
 
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class NotificationsService {
-  async notifyEmail({ email }: NotifyEmailDto) {
-    console.log('===> notifyEmail', email);
+  constructor(private readonly configService: ConfigService) {}
+
+  // private readonly transporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //     type: 'OAuth2',
+  //     user: this.configService.get('SMTP_USER'),
+  //     clientId: this.configService.get('GOOGLE_OAUTH_CLIENT_ID'),
+  //     clientSecret: this.configService.get('GOOGLE_OAUTH_CLIENT_SECRET'),
+  //     refreshToken: this.configService.get('GOOGLE_OAUTH_CLIENT_REFRESH_TOKEN'),
+  //   },
+  // });
+
+  private readonly transporter = nodemailer.createTransport({
+    host: this.configService.get('MAIL_HOST'),
+    port: this.configService.get('MAIL_PORT'),
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: this.configService.get('MAIL_USER'),
+      pass: this.configService.get('MAIL_PASSWORD'),
+    },
+  });
+
+  async notifyEmail({ email, text }: NotifyEmailDto) {
+    await this.transporter.sendMail({
+      from: this.configService.get('SMTP_USER'),
+      to: email,
+      subject: 'Sleepr notification',
+      text,
+    });
   }
 }
